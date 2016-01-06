eed190ebd3d16e02bef9332fbfdb41a5c85a3f4c
warrior1208 <narang_manik@rocketmail.com>
2016-01-06 12:30:31 +0530

    Use the Slim-ified defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9100/bluetooth
 
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
-TARGET_KERNEL_CONFIG := cyanogenmod_i9100_defconfig
+TARGET_KERNEL_CONFIG := slim_i9100_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-15.05/bin/arm-linux-gnueabihf-
 
 # assert
